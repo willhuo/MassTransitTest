4ac2b113f9652ba54eb556c38d9a0dd67018b202
willhuo <willhuo@outlook.com>
2024-03-20 11:57:21 +0800

    masstransit测试提交

---
+++ src/MassTransitTest/MassTransitTest/Mqs/Interfaces/ITerminalRun.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TN.Message
+{
+    /// <summary>
+    /// 导航设备运行启停消息
+    /// </summary>
+    public class ITerminalRun
+    {
+        /// <summary>
+        /// 终端编号
+        /// </summary>
+        public string TerminalNo { get; set; }
+
+        /// <summary>
+        /// 消息产生的时间,处理时如果超过1分钟，就无视
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// true 启动运行 false 停止运行
+        /// </summary>
+        public bool Run { get; set; }
+    }
+}
---
+++ src/MassTransitTest/MassTransitTest/Program.cs
@@ -0,0 +1,64 @@
+﻿using MassTransit;
+using MassTransitTest.Mqs;
+using TN.Message;
+
+namespace MassTransitTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host(new Uri("rabbitmq://localhost/"), h =>
+                {
+                    h.Username("admin");
+                    h.Password("admin");
+                });
+
+                cfg.ReceiveEndpoint(ep =>
+                {
+                    ep.Consumer<YourMessageConsumer>();
+                });
+
+                //cfg.ReceiveEndpoint("your_queue_name", ep =>
+                //{
+                //    ep.Consumer<YourMessageConsumer>();
+                //});
+            });
+
+            busControl.Start();
+            Console.WriteLine("bus start");
+
+            try
+            {
+                Console.WriteLine("ready to pub mq msg");
+                while (true)
+                {                   
+                    //busControl.Publish(new ITerminalRun
+                    //{
+                    //    TerminalNo = "0001282200000003",
+                    //    Time=DateTime.Now,
+                    //    Run=true
+                    //});
+                    //Console.WriteLine("mq msg pub over");
+                    //Console.WriteLine("-------------------------");
+                    Task.Delay(5000).Wait();
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+
+    public class YourMessageConsumer : IConsumer<ITerminalRun>
+    {
+        public Task Consume(ConsumeContext<ITerminalRun> context)
+        {
+            Console.WriteLine($"Received message: {context.Message.TerminalNo}");
+            return Task.CompletedTask;
+        }
+    }
+}
